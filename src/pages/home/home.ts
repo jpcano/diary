908410cb427e6afc7bd5bf12b909912c64aeb3f8
Jesus Caño <jcanovel@gmail.com>
2017-02-16 12:48:20 +0100

    ngStyle

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -10,11 +10,19 @@ export class HomePage {
 
     talk = {
 	name: 'ionic2',
-	speaker: 'jpcano'
+	speaker: 'jpcano',
+	category: 'workshop'
     };
 
     constructor(public navCtrl: NavController) {
 
     }
 
+    style(talk) {
+	return {
+	    'border-color': talk.category == 'workshop' ? 'purple' : 'red',
+	    'border-left': '2px solid #00274F',
+	    'padding-left': '10px'
+	}
+    }
 }
